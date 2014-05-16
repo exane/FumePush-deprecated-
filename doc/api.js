6ece57c2f65f4570a9beef7f88ab5f9e2f2ff823
exane <raco0n@gmx.de>
2014-05-16 20:43:56 +0200

    added yuidoc and documentation

---
+++ doc/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Channel",
+        "FumePush"
+    ],
+    "modules": [
+        "Channel",
+        "FumePush"
+    ],
+    "allModules": [
+        {
+            "displayName": "Channel",
+            "name": "Channel"
+        },
+        {
+            "displayName": "FumePush",
+            "name": "FumePush"
+        }
+    ]
+} };
+});
