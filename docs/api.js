29ea55b8d4fd7488c918ba413d60c3c3af42ea4d
exane <raco0n@gmx.de>
2014-05-17 11:32:19 +0200

    changed yuidoc theme, changed npm dependencies

---
+++ docs/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Channel",
+        "FumePush"
+    ],
+    "modules": [
+        "FumePush",
+        "FumePush.Channel"
+    ],
+    "allModules": [
+        {
+            "displayName": "FumePush",
+            "name": "FumePush"
+        },
+        {
+            "displayName": "FumePush.Channel",
+            "name": "FumePush.Channel"
+        }
+    ]
+} };
+});
